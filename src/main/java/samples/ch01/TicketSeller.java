b171c3c87e937111904253070f8681bc744c5ee6
Gyumin Kim <starkying@gmail.com>
2021-07-30 00:54:44 +0900

    1장: TicketOffice 캡슐화

--- src/main/java/samples/ch01/TicketSeller.java
+++ src/main/java/samples/ch01/TicketSeller.java
@@ -12,8 +12,6 @@ public class TicketSeller {
     }
 
     public void sellTo(Audience audience) {
-        Ticket ticket = ticketOffice.getTicket();
-        Long amount = audience.buy(ticket);
-        ticketOffice.plusAmount(amount);
+        ticketOffice.sellTicketTo(audience);
     }
 }
