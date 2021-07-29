d4c685bbcb8fe3137784e43dd7d98727c5c758e5
Gyumin Kim <starkying@gmail.com>
2021-07-30 00:37:44 +0900

    1장: Bag 사용되지 않는 메서드 삭제

--- src/main/java/samples/ch01/Bag.java
+++ src/main/java/samples/ch01/Bag.java
@@ -18,19 +18,11 @@ public class Bag {
         return invitation != null;
     }
 
-    public boolean hasTicket() {
-        return ticket != null;
-    }
-
     public void setTicket(Ticket ticket) {
         this.ticket = ticket;
     }
 
     public void minusAmount(Long amount) {
         this.amount -= amount;
-    }
-
-    public void plusAmount(Long amount) {
-        this.amount += amount;
     }
 }
