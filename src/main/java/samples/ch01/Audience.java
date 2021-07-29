0fa918d63a4b27c624bf32938448c916bc59f8b4
Gyumin Kim <starkying@gmail.com>
2021-07-29 10:05:56 +0900

    1장: 초기 설계

---
+++ src/main/java/samples/ch01/Audience.java
@@ -0,0 +1,13 @@
+package samples.ch01;
+
+public class Audience {
+    private Bag bag;
+
+    public Audience(final Bag bag) {
+        this.bag = bag;
+    }
+
+    public Bag getBag() {
+        return bag;
+    }
+}
