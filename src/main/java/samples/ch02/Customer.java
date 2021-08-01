b582e5c0461e3e52af907fc60ae961ca873b758e
Gyumin Kim <starkying@gmail.com>
2021-08-01 10:30:11 +0900

    2장: Customer 추가

---
+++ src/main/java/samples/ch02/Customer.java
@@ -0,0 +1,11 @@
+package samples.ch02;
+
+public class Customer {
+    private String name;
+    private String id;
+
+    public Customer(final String name, final String id) {
+        this.name = name;
+        this.id = id;
+    }
+}
